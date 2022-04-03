19884415e0296a23d6dc43f6ce87976879ddea37
GPSiteInfo <102870230+GPSiteInfo@users.noreply.github.com>
2022-04-04 04:55:53 +0600

    Add files via upload

---
+++ GPSiteInfoBot/modules/gps.py
@@ -0,0 +1,44 @@
+from geopy.geocoders import Nominatim
+from telegram import Location, ParseMode
+from telegram.ext import CommandHandler, run_async
+
+from GPSiteInfoBot import dispatcher
+from GPSiteInfoBot.modules.helper_funcs.chat_status import user_admin
+
+GMAPS_LOC = "https://maps.googleapis.com/maps/api/geocode/json"
+
+
+dispatcher.run_async
+@user_admin
+def gps(update, context, *args, **kwargs):
+
+    args = context.args
+    update.effective_message
+    if len(args) == 0:
+        update.effective_message.reply_text(
+            "That was a funny joke, but no really, put in a location"
+        )
+    try:
+        geolocator = Nominatim(user_agent="SkittBot")
+        location = " ".join(args)
+        geoloc = geolocator.geocode(location)
+        chat_id = update.effective_chat.id
+        lon = geoloc.longitude
+        lat = geoloc.latitude
+        the_loc = Location(lon, lat)
+        gm = "https://www.google.com/maps/search/{},{}".format(lat, lon)
+        dispatcher.bot.send_location(chat_id, location=the_loc)
+        update.message.reply_text(
+            "Open with: [Google Maps]({})".format(gm),
+            parse_mode=ParseMode.MARKDOWN)
+    except AttributeError:
+        update.message.reply_text("I can't find that")
+
+
+GPS_HANDLER = CommandHandler("gps", gps)
+
+dispatcher.add_handler(GPS_HANDLER)
+
+
+__command_list__ = ["gps"]
+__handlers__ = [GPS_HANDLER]
---
+++ GPSiteInfoBot/modules/media_tools.py
@@ -0,0 +1,16 @@
+__help__ = """
+
+*✳ Carbon Maker:*
+`/carbon` <text> [or reply].
+
+*✳ Image Tools:*
+`/img` <text>: perform a image search.
+`/getqr` <reply to image>: Read QR code.
+`/makeqr` <reply to text>: Make QR code.
+
+*✳ Logo Maker:*
+`/logo` text:  Create your logo with yellow font and black background. 
+`/wlogo` text: Create your logo with white font and black background.
+
+ """
+__mod_name__ = "Media Tools"
