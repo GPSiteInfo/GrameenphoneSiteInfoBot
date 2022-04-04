3896e116abeed4fe38feca73407c5f99d7b879e5
GPSiteInfo <102870230+GPSiteInfo@users.noreply.github.com>
2022-04-04 14:02:24 +0600

    Create BGKRP1.py

---
+++ GPSiteInfoBot/modules/BGKRP1.py
@@ -0,0 +1,12 @@
+__help__ = """
+
+Site Code : BGKRP1
+
+Location  : Karapara,Dashani,Bagerhat,Bagerhat .
+
+GPS Location : (Click Here)[https://www.google.com/maps/dir//22.63861,89.76517/@22.6901757,89.7167656,11z/data=!4m5!4m4!1m1!4e2!1m0!3e0?gl=bd]
+
+ISS_TYPE : Non-shared.
+
+ """
+__mod_name__ = "BGKRP1"
