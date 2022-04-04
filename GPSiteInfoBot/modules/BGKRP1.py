726e48f822b7eda45c39b883a6521bbded90623d
GPSiteInfo <102870230+GPSiteInfo@users.noreply.github.com>
2022-04-04 22:35:54 +0600

    Update BGKRP1.py

--- GPSiteInfoBot/modules/BGKRP1.py
+++ GPSiteInfoBot/modules/BGKRP1.py
@@ -1,12 +1,12 @@
 from GPSiteInfoBot import dispatcher
 
-
 from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import escape_markdown
 from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
 
 SITE_INFO_TEXT = """
+[ ](https://telegra.ph/file/3a093be342d6ff3e77221.jpg)
 📶 Site Code : BGKRP1
 
 📶 Site Name : Karapara 1
@@ -16,7 +16,7 @@ SITE_INFO_TEXT = """
 🌏 Site Location : Karapara, Dashani, Bagerhat.
 """
 
-SITE_LOCATION_BUTTON = [[InlineKeyboardButton(text="Go to site with maps", url="https://www.google.com/maps/place/22%C2%B038'19.0%22N+89%C2%B045'54.6%22E/@22.63861,89.76517,17z?gl=bd"),]]
+SITE_LOCATION_BUTTON = [[InlineKeyboardButton(text="🌍 Go to site with maps", url="https://www.google.com/maps/place/22%C2%B038'19.0%22N+89%C2%B045'54.6%22E/@22.63861,89.76517,17z?gl=bd"),]]
 
 
 dispatcher.run_async
