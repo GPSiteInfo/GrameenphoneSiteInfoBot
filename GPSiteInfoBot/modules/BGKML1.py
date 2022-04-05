4be18973e285178fa764cceb79f82bda35936514
GPSiteInfo <102870230+GPSiteInfo@users.noreply.github.com>
2022-04-05 10:44:51 +0600

    Create BGKML1.py

---
+++ GPSiteInfoBot/modules/BGKML1.py
@@ -0,0 +1,33 @@
+from GPSiteInfoBot import dispatcher
+
+from GPSiteInfoBot.modules.helper_funcs.filters import CustomFilters
+
+from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
+from telegram.ext import CallbackContext, CommandHandler
+from telegram.utils.helpers import escape_markdown
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
+
+Tower_Photo = "https://telegra.ph/file/3a093be342d6ff3e77221.jpg"
+
+SITE_INFO_TEXT = """
+📶 Site Code: BGKML1
+
+📶 Site Name: Pobontola Bazar
+
+📶 ISS_TYPE: Non-shared .
+
+📶 Site Location: Pobontola Bazar, Rampal, Bagerhat.
+
+📶 Nearby anyone phone number: 01910500562
+"""
+
+SITE_LOCATION_BUTTON = [[InlineKeyboardButton(text="✳ Go To BGKML1 Site With Maps", url="https://www.google.com/maps/dir//22.62167,89.68206"),]]
+
+
+def bgkrp1(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(Tower_Photo, SITE_INFO_TEXT, parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(SITE_LOCATION_BUTTON))
+
+
+bgkml1_handler = CommandHandler("bgkml1", bgkml1, filters=CustomFilters.authorized_user, run_async=True)
+dispatcher.add_handler(bgkml1_handler)
