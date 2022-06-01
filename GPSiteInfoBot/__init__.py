b69860c410344b3e151b29fe188e472d352b8dfb
GPSiteInfo <102870230+GPSiteInfo@users.noreply.github.com>
2022-06-01 21:26:23 +0600

    Update __init__.py

--- GPSiteInfoBot/__init__.py
+++ GPSiteInfoBot/__init__.py
@@ -26,6 +26,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 
 
 try:
+    ALLOW_EXCL = "True"
     API_HASH = os.environ.get("API_HASH", None)
     API_ID = os.environ.get("API_ID", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
