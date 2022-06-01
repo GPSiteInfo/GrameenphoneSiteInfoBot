8d6cedb2554d3c6dc2c63d801e0119e5f068dd54
GPSiteInfo <102870230+GPSiteInfo@users.noreply.github.com>
2022-06-01 18:52:40 +0600

    Update __init__.py

--- GPSiteInfoBot/__init__.py
+++ GPSiteInfoBot/__init__.py
@@ -26,15 +26,10 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 
 
 try:
-    API_ID = 2076846
-    API_HASH = "a7c38b63155953f8c529718a3ac0003a"
-    BOT_TOKEN = "5022788742:AAEk4DvdxA1mfBbPFs8xvB65rbJJq1YKBlE"
+    API_HASH = os.environ.get("API_HASH", None)
+    API_ID = os.environ.get("API_ID", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     CERT_PATH = os.environ.get("CERT_PATH")
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
-    INFOPIC = bool(os.environ.get("INFOPIC", False))
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     OWNER_ID = os.environ.get("OWNER_ID", None)
     PORT = int(os.environ.get("PORT", 5000))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "AtrociousBotSupport")
