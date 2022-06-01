754589b9774a31cf39b2cde107eed0013296d06b
GPSiteInfo <102870230+GPSiteInfo@users.noreply.github.com>
2022-06-01 19:52:49 +0600

    Update error_handler.py

--- GPSiteInfoBot/modules/error_handler.py
+++ GPSiteInfoBot/modules/error_handler.py
@@ -8,7 +8,7 @@ import pretty_errors
 import io
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
-from GPSiteInfoBot import dispatcher OWNER_ID
+from GPSiteInfoBot import dispatcher, OWNER_ID
 
 pretty_errors.mono()
 
